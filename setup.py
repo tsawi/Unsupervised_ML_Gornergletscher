d2d57866b5aa0be23c51822007d4b0b4b58eda94
Theresa Sawi <theresasawi@gmail.com>
2021-07-22 15:13:33 -0400

    first push

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Unsupervised learning oat Gorner Glacier',
+    author='T. Sawi',
+    license='MIT',
+)
